11e756e3db1db69af8b2176db53861c22677ded1
Kakashi <49955287+KakashiHatake324@users.noreply.github.com>
2022-10-16 19:04:37 -0400

    Delete dist directory

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var electron_1 = require("electron");
-var Main_1 = require("./Main");
-Main_1.default.main(electron_1.app, electron_1.BrowserWindow);
-//# sourceMappingURL=app.js.map
